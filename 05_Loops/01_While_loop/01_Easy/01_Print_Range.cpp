eacea16377a375bcb8c770e76ae0652791998ddf
mohamed <abdeltwab@gmail.com>
2024-02-09 23:42:49 +0200

    loops

--- 05_Loops/01_While_loop/01_Easy/01_Print_Range.cpp
+++ 05_Loops/01_While_loop/01_Easy/01_Print_Range.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
+    int start, end;
 
+    cin>>start>>end;
+
+    while(start <= end)
+    {
+        cout<<start<<endl;
+        start++;
+    }
 
     return 0;
 }
